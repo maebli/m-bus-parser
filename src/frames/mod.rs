fc3bcd41f1e2d35c0640f7b67b8978284efc51d0
heroichornet <schnipper@gmail.com>
2023-11-17 00:24:00 +0100

    adding example, parsing funciton field

--- src/frames/mod.rs
+++ src/frames/mod.rs
@@ -4,19 +4,47 @@ pub enum FrameType<'a> {
         character: u8
     },
     ShortFrame{
-        function: u8, 
+        function: Function, 
         address: u8
     },
     LongFrame{
-        function: u8,
+        function: Function,
         address: u8,
         control_information: u8, 
         data: &'a [u8]},
     ControlFrame{
-        function: u8, 
+        function: Function, 
         address: u8, 
         control_information: u8
     },
+}
+
+#[derive(Debug,PartialEq)]
+pub enum Function{
+    SND_NK,
+    SND_UD{FCB: bool},
+    REQ_UD2{FCB: bool},
+    REQ_UD1{FCB: bool},
+    RSP_UD{ACD:bool, DFC:bool}
+}
+
+impl Function {
+    fn from(byte: u8) -> Function {
+        match byte {
+            0x40 => Function::SND_NK,
+            0x53 => Function::SND_UD{FCB: false},
+            0x73 => Function::SND_UD{FCB: true},
+            0x5B => Function::REQ_UD2{FCB: false},
+            0x7B => Function::REQ_UD2{FCB: true},
+            0x5A => Function::REQ_UD1{FCB: false},
+            0x7A => Function::REQ_UD1{FCB: true},
+            0x08 => Function::RSP_UD{ACD: false, DFC: false},
+            0x18 => Function::RSP_UD{ACD: false, DFC: true},
+            0x28 => Function::RSP_UD{ACD: true, DFC: false},
+            0x38 => Function::RSP_UD{ACD: true, DFC: true},
+            _ => panic!("Invalid function byte: {}", byte),
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -79,12 +107,12 @@ pub fn parse_frame(data: &[u8])  -> Result<FrameType, FrameError> {
             let control_field = data[4];
             match control_field {
                 0x53 => Ok(FrameType::ControlFrame{
-                    function: data[4], 
+                    function: Function::from(data[4]), 
                     address: data[5],
                     control_information: data[6]
                 }),
                 _ => Ok(FrameType::LongFrame{
-                    function: data[4],
+                    function: Function::from(data[4]),
                     address: data[5],
                     control_information: data[6],
                     data: &data[7..data.len() - 2],
@@ -95,7 +123,7 @@ pub fn parse_frame(data: &[u8])  -> Result<FrameType, FrameError> {
             validate_checksum(&data[1..])?;
             if data.len() == 5 && data[4] == 0x16 {
                 Ok(FrameType::ShortFrame{
-                    function: data[1],
+                    function: Function::from(data[1]),
                     address: data[2],
                 })
             } else {
@@ -138,15 +166,39 @@ mod tests {
         let single_character_frame = vec![0xE5];
         let short_frame = vec![0x10, 0x7B, 0x8b, 0x06,0x16];
         let control_frame = vec![0x68, 0x03, 0x03, 0x68, 0x53, 0x01, 0x00, 0x54, 0x16];
+        
+        let example = vec![
+            0x68, 0x4D, 0x4D, 0x68, 0x08, 0x01, 0x72, 0x01, 0x00, 0x00, 0x00, 0x96, 0x15, 0x01, 
+            0x00, 0x18, 0x00, 0x00, 0x00, 0x0C, 0x78, 0x56, 0x00, 0x00, 0x00, 0x01, 0xFD, 0x1B, 
+            0x00, 0x02, 0xFC, 0x03, 0x48, 0x52, 0x25, 0x74, 0x44, 0x0D, 0x22, 0xFC, 0x03, 0x48, 
+            0x52, 0x25, 0x74, 0xF1, 0x0C, 0x12, 0xFC, 0x03, 0x48, 0x52, 0x25, 0x74, 0x63, 0x11, 
+            0x02, 0x65, 0xB4, 0x09, 0x22, 0x65, 0x86, 0x09, 0x12, 0x65, 0xB7, 0x09, 0x01, 0x72, 
+            0x00, 0x72, 0x65, 0x00, 0x00, 0xB2, 0x01, 0x65, 0x00, 0x00, 0x1F, 0xB3, 0x16,
+        ];
+        
 
 
         assert_eq!(parse_frame(&single_character_frame), Ok(FrameType::SingleCharacter{character: 0xE5}));
-        assert_eq!(parse_frame(&short_frame), Ok(FrameType::ShortFrame{function: 0x7B, address: 0x8B}));
+        assert_eq!(parse_frame(&short_frame), Ok(FrameType::ShortFrame{function: Function::from(0x7B), address: 0x8B}));
         assert_eq!(parse_frame(&control_frame), Ok(FrameType::ControlFrame {
-            function: 0x53,
+            function: Function::from(0x53),
             address: 0x01,
             control_information: 0x00,
         }));
+
+        assert_eq!(parse_frame(&example),Ok(FrameType::LongFrame {  
+            function: Function::from(8), 
+            address: 1, 
+            control_information: 114, 
+            data: &[
+                1, 0, 0, 0, 150, 21, 1, 0, 24, 0,
+                0, 0, 12, 120, 86, 0, 0, 0, 1, 253, 
+                27, 0, 2, 252, 3, 72, 82, 37, 116, 
+                68, 13, 34, 252, 3, 72, 82, 37, 116, 
+                241, 12, 18, 252, 3, 72, 82, 37, 116, 
+                99, 17, 2, 101, 180, 9, 34, 101, 134, 
+                9, 18, 101, 183, 9, 1, 114, 0, 114,
+                 101, 0, 0, 178, 1, 101, 0, 0, 31] }));
     }
 
 }
