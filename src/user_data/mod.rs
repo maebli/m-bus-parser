9f00da3cd5a2a84f4f855a0e3e366b10eddfb256
heroichornet <schnipper@gmail.com>
2023-11-21 00:32:13 +0100

    some new additions

--- src/user_data/mod.rs
+++ src/user_data/mod.rs
@@ -2,6 +2,33 @@
 //! User data is part of the application layer
 
 #[derive(Debug, PartialEq)]
+pub struct StatusField {
+    pub counter_binary_signed: bool,
+    pub counter_fixed_date: bool,     
+    pub power_low: bool,             
+    pub permanent_error: bool,       
+    pub temporary_error: bool,       
+    pub manufacturer_specific_1: bool, 
+    pub manufacturer_specific_2: bool,
+    pub manufacturer_specific_3: bool,
+}
+
+impl StatusField {
+    pub fn from(byte: u8) -> Self {
+        StatusField {
+            counter_binary_signed: byte & 0b00000001 != 0,
+            counter_fixed_date: byte & 0b00000010 != 0,
+            power_low: byte & 0b00000100 != 0,
+            permanent_error: byte & 0b00001000 != 0,
+            temporary_error: byte & 0b00010000 != 0,
+            manufacturer_specific_1: byte & 0b00100000 != 0,
+            manufacturer_specific_2: byte & 0b01000000 != 0,
+            manufacturer_specific_3: byte & 0b10000000 != 0,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 pub enum Direction {
     SlaveToMaster,
     MasterToSlave,
@@ -137,9 +164,6 @@ pub struct IdentificationNumber {
 }
 
 impl IdentificationNumber {
-    pub fn new(number: u32) -> Self {
-        IdentificationNumber { number }
-    }
 
     pub fn from_bcd_digits(digits: [u8; 8]) -> Result<Self, IdentificationNumberError> {
         let mut number = 0u32;
@@ -161,20 +185,101 @@ pub enum UserDataBlock {
     FixedDataStructure{
         IdentificationNumber: IdentificationNumber,
         AccessNumber: u8,
-        Status: u8,
+        Status: StatusField,
         Medium: u8,
         Counter1: u32,
         Counter2: u32,
     }
 }
+#[derive(Debug, Clone, Copy)]
+pub enum Medium {
+    Other,
+    Oil,
+    Electricity,
+    Gas,
+    Heat,
+    Steam,
+    HotWater,
+    Water,
+    HCA,
+    Reserved,
+    GasMode2,
+    HeatMode2,
+    HotWaterMode2,
+    WaterMode2,
+    HCAMode2,
+    ReservedMode2,
+    HeatVolumeReturn,
+    HeatCostAllocator,
+    CompressedAir,
+    CoolingLoadMeterReturn,
+    CoolingLoadMeterFlow,
+    HeatVolumeFlow,
+    HeatCoolingLoadMeter,
+    BusSystem,
+    UnknownMedium,
+    ColdWater,
+    DualWater,
+    Pressure,
+    ADConverter,
+}
 
-pub enum UserDataError {
-    InvalidControlInformation,
-    InvalidUserData,
+impl Medium {
+    pub fn from_byte(byte: u8) -> Self {
+        match byte {
+            0x00 => Medium::Other,
+            0x01 => Medium::Oil,
+            0x02 => Medium::Electricity,
+            0x03 => Medium::Gas,
+            0x04 => Medium::Heat,
+            0x05 => Medium::Steam,
+            0x06 => Medium::HotWater,
+            0x07 => Medium::Water,
+            0x08 => Medium::HCA,
+            0x09 => Medium::Reserved, // Note: Reserved for 0x09 from the first set
+            0x0A => Medium::GasMode2,
+            0x0B => Medium::HeatMode2,
+            0x0C => Medium::HotWaterMode2,
+            0x0D => Medium::WaterMode2,
+            0x0E => Medium::HCAMode2,
+            0x0F => Medium::ReservedMode2,
+            // Unique mediums from the second set
+            0x10 => Medium::Reserved, // Reserved range
+            0x11 => Medium::Reserved, // Reserved range
+            0x12 => Medium::Reserved, // Reserved range
+            0x13 => Medium::Reserved, // Reserved range
+            0x14 => Medium::Reserved, // Reserved range
+            0x15 => Medium::Reserved, // Reserved range
+            0x16 => Medium::ColdWater,
+            0x17 => Medium::DualWater,
+            0x18 => Medium::Pressure,
+            0x19 => Medium::ADConverter,
+            // Extended reserved range from the second set
+            0x20..=0xFF => Medium::Reserved,
+            _ => Medium::UnknownMedium,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct MeasuredMedium {
+    pub medium: Medium,
+}
+
+impl MeasuredMedium {
+    pub fn new(byte: u8) -> Self {
+        MeasuredMedium {
+            medium: Medium::from_byte(byte),
+        }
+    }
 }
 
 pub fn parse_user_data(data: &[u8]) -> Result<UserDataBlock, ApplicationLayerError> {
     
+    if data.is_empty() {
+        return Err(ApplicationLayerError::MissingControlInformation);
+    }
+
     let control_information = ControlInformation::from(data[0])?;
 
     match control_information {
@@ -233,5 +338,13 @@ mod tests {
         assert_eq!(ControlInformation::from(0xB6), Ok(ControlInformation::StartSoftwareTest(Direction::MasterToSlave)));
         assert_eq!(ControlInformation::from(0x90), Ok(ControlInformation::HashProcedure(0, Direction::MasterToSlave)));
         assert_eq!(ControlInformation::from(0x91), Ok(ControlInformation::HashProcedure(1, Direction::MasterToSlave)));
+    }
+
+    #[test]
+    fn test_reset_subcode(){
+        // Application layer of frame | 68 04 04 68 | 53 FE 50 | 10 | B1 16
+        let data = [ 0x50, 0x10];
+        let result = parse_user_data(&data);
+        assert_eq!(result, Ok(UserDataBlock::ResetAtApplicationLevel{subcode: ApplicationResetSubcode::All(0x10)}));
     }
 }
