fcc6c78f0158dabf79c599f23ef317017c608dd8
heroichornet <schnipper@gmail.com>
2024-02-10 12:16:35 +0100

    fixing tests

--- tests/test.rs
+++ tests/test.rs
@@ -10,17 +10,18 @@ mod tests {
 
     #[test]
     fn test_print_hex_files() {
-        for entry in WalkDir::new(".")
+        /* parses all the good examples, shouldn't throw any errors. */
+        for entry in WalkDir::new("./test-frames")
             .into_iter()
             .filter_map(|e| e.ok())
             .filter(|e| e.path().extension().map_or(false, |ext| ext == "hex"))
         {
 
-            let xml_path = entry.path().with_extension("xml");
             let contents = fs::read_to_string(entry.path())
                 .expect("Something went wrong reading the file");
+            println!("Path: {}", entry.path().display());
             println!("Input:\n{}", contents);
-            // contents to bytes array
+
             let contents = contents.trim().replace(" ", "");
             let bytes = hex::decode(contents).unwrap();
             let frame = parse_frame(bytes.as_slice()).unwrap();
