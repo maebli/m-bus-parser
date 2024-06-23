da399594a8dff997a4730c34489f6a5bc3af6930
heroichornet <schnipper@gmail.com>
2024-06-23 09:52:43 +0200

    adding cortex-m from example and removing most examples

---
+++ examples/cortex-m/examples/hello.rs
@@ -0,0 +1,18 @@
+//! Prints "Hello, world!" on the host console using semihosting
+
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
+
+#[entry]
+fn main() -> ! {
+    hprintln!("{:?}", mbus_data);
+
+    debug::exit(debug::EXIT_SUCCESS);
+
+    loop {}
+}
