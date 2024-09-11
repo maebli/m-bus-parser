a1aedafd4151495d61876ef35cbc79fad01be29a
Michael Aebli <michael.aebli@gwf.ch>
2024-09-11 09:06:55 +0200

    removing duplicate from example

--- examples/example_parsing_user_data.rs
+++ examples/example_parsing_user_data.rs
@@ -2,7 +2,6 @@ use m_bus_parser::user_data::DataRecords;
 
 fn main() {
     /* Data block 1: unit 0, storage No 0, no tariff, instantaneous volume, 12565 l (24 bit integer) */
-    /* Data block 2: unit 0, storage No 0, no tariff, instantaneous volume, 12565 l (24 bit integer) */
     let data = vec![0x03, 0x13, 0x15, 0x31, 0x00, 0x03, 0x13, 0x15, 0x31, 0x00];
     let result = DataRecords::try_from(data.as_slice());
     assert!(result.is_ok());
